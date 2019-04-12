1c397d93690fa2f602376971cafcea1c639e2d73
Tiago Mota <tiagomota@Tiagos-MacBook-Pro.local>
2019-04-11 21:27:39 -0400

    1-10Done

--- problems/problem10.js
+++ problems/problem10.js
@@ -1,9 +1,9 @@
 let verifyEquals = require('./verify-equals.js');
 
 // we need 5 test cases.
-let inputs = [];
+let inputs = ['hello world', 'TAKE ME TO YOUR LEADER', 'aNYONE SEEN MY CAPS LOCK?', 'i ThiNk My KeYBoArD hAS IssUEs', ' What about the space in the beginning of this sentence?'];
 
-let outputs = [];
+let outputs = ['Hello world', 'Take me to your leader', 'Anyone seen my caps lock?', 'I think my keyboard has issues', ' what about the space in the beginning of this sentence?'];
 
 /*
 Make this function return the input string, capitalized. You must use a for loop. For example:
@@ -15,7 +15,15 @@ HINT:
    - Use a for loop to capitalize the words one by one
    - Use the toUpperCase string method
 */
-function f(str) {}
+function f(str) {
+  let split = str.split('');
+  let cap = [];
+  cap.push(str.charAt(0).toUpperCase());
+  for (l = 1; l < str.length; l++) {
+    cap.push(split[l].toLowerCase());
+  }
+  return(cap.join(''));
+}
 
 //This function runs a test. You do not need to change any code under here
 function runTest(i) {
